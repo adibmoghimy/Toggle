149d62eb1bf5af6fbe4335fa19ed5d3ca77cfce5
adibmoghimy <38736839+adibmoghimy@users.noreply.github.com>
2018-09-17 13:43:13 +0430

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+mongoose.connect('mongodb://adib:adib681373@ds253960.mlab.com:53960/gql-adibbb');
+mongoose.connection.once('open', () => {
+    console.log('connected to datebase');
+})
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
+});
